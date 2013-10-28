76b135d5d06c628f5252a1c45dc13b0f4d09934a
Kioksy <ante89@gmail.com>
2013-10-28 19:38:30 +0100

    Artist object added

--- src/org/jspotsearch/storageobjects/Album.java
+++ src/org/jspotsearch/storageobjects/Album.java
@@ -1,5 +1,68 @@
 package org.jspotsearch.storageobjects;
 
-public class Album {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Album {
+	
+	private String album;
+	private String artist;
+	private List<Song> tracks = new ArrayList<Song>();
+	private String yearOfRelease;
+	
+	/**
+	 * The name of the album
+	 * @return the name of the album
+	 */
+	public String getAlbum() {
+		return album;
+	}
+	
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	
+	/**
+	 * The name of the artist
+	 * @return the name of the artist
+	 */
+	public String getArtist() {
+		return artist;
+	}
+	
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	
+	/**
+	 * All the songs in the album
+	 * @return all the songs as Song object
+	 */
+	public List<Song> getTracks() {
+		return tracks;
+	}
+	
+	public void setTracks(List<Song> tracks) {
+		this.tracks = tracks;
+	}
+	
+	/**
+	 * The year when the album was released
+	 * @return year as string
+	 */
+	public String getYearOfRelease() {
+		return yearOfRelease;
+	}
+	
+	
+	public void setYearOfRelease(String yearOfRelease) {
+		this.yearOfRelease = yearOfRelease;
+	}
+
+	@Override
+	public String toString(){
+		return "This album ( " + this.getAlbum() + " ) by: " + this.getArtist() +
+			"has a total of: " + this.getTracks().size() + " songs.";
+	}
 }
+
